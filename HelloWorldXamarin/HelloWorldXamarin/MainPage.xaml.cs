73fccc96e8ae87f84261a9ca1b36ad07c76e0483
kanarekha <kanarekha8@gmail.com>
2021-07-10 19:20:04 +0700

    first commit

---
+++ HelloWorldXamarin/HelloWorldXamarin/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorldXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+
+        void ClickCounter(object sender, System.EventArgs e)
+
+        {
+
+            count++;
+
+            ((Button)sender).Text = $"You clicked {count} times.";
+
+        }
+    }
+}
